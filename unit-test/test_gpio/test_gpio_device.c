994d59f9faa30faa2c0396c97c51b4f3d6d297fc
Ivan Bakula <wamreu@gmail.com>
2023-08-14 13:35:46 +0200

    Fix gpio unit test

--- unit-test/test_gpio/test_gpio_device.c
+++ unit-test/test_gpio/test_gpio_device.c
@@ -62,21 +62,28 @@ void test_gpio_pin_config(void)
 
   /* CFGLR */
   const uint32_t pa0_shift = 0;
+  const uint32_t pa0_mask = 0x000f;
   const uint32_t pb5_shift = 20;
-
+  const uint32_t pb5_mask = (0xf << pb5_shift);
   /* CFGHR */
   const uint32_t pc8_shift = 0;
+  const uint32_t pc8_mask = 0x000f;
   const uint32_t pd13_shift = 20;
+  const uint32_t pd13_mask = (0x000f << pd13_shift);
 
+  mmio_and_writedw_Expect(_GPIO_REGISTER(GPIOA_BASE, R32_GPIO_CFGLR), ~pa0_mask);
   mmio_or_writedw_Expect(_GPIO_REGISTER(GPIOA_BASE, R32_GPIO_CFGLR), output_mode_50mhz_push_pull_alt << pa0_shift);
   gpio_pin_config(PA0, GPIO_Mode_Output_50MHz, GPIO_Output_PushPullAlt);
 
+  mmio_and_writedw_Expect(_GPIO_REGISTER(GPIOB_BASE, R32_GPIO_CFGLR), ~pb5_mask);
   mmio_or_writedw_Expect(_GPIO_REGISTER(GPIOB_BASE, R32_GPIO_CFGLR), input_mode_float << pb5_shift);
   gpio_pin_config(PB5, GPIO_Mode_Input, GPIO_Input_Float);
 
+  mmio_and_writedw_Expect(_GPIO_REGISTER(GPIOC_BASE, R32_GPIO_CFGHR), ~pc8_mask);
   mmio_or_writedw_Expect(_GPIO_REGISTER(GPIOC_BASE, R32_GPIO_CFGHR), output_mode_10mhz_open_drain << pc8_shift);
   gpio_pin_config(PC8, GPIO_Mode_Output_10MHz, GPIO_Output_OpenDrain);
 
+  mmio_and_writedw_Expect(_GPIO_REGISTER(GPIOD_BASE, R32_GPIO_CFGHR), ~pd13_mask);
   mmio_or_writedw_Expect(_GPIO_REGISTER(GPIOD_BASE, R32_GPIO_CFGHR), input_mode_pull_up_down << pd13_shift);
   gpio_pin_config(PD13, GPIO_Mode_Input, GPIO_Input_PullUpDown);
 }
@@ -92,7 +99,7 @@ void test_gpio_pin_output(void)
 
 void test_gpio_pin_input(void)
 {
-  mmio_and_readw_ExpectAndReturn(_GPIO_REGISTER(GPIOA_BASE, R32_GPIO_INDR), U16_BIT(15), 1);
+  mmio_and_readw_ExpectAndReturn(_GPIO_REGISTER(GPIOA_BASE, R32_GPIO_INDR), U16_BIT(15), U16_BIT(15));
   TEST_ASSERT_EQUAL(1, gpio_pin_input(PA15));
 }
 
