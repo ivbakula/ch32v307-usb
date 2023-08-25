0249919d770d6031308e30a67c89e272bfb89070
ivbakula <84186651+ivbakula@users.noreply.github.com>
2023-08-25 14:14:16 +0200

    46-implement-console (#51)
    * 46-implement-console
    
    * printf stolen from https://github.com/mpaland/printf
    
    * Apply codestyle
    
    * Replace old printf with new one and dont link agains libc

--- boot/boot.c
+++ boot/boot.c
@@ -4,6 +4,7 @@
 #include "../src/dvp_device.h"
 #include "dvp_interface.h"
 #include "mmem.h"
+#include "printf.h"
 #include "time.h"
 #include "uart_interface.h"
 
@@ -12,30 +13,30 @@ extern uint32_t _susrstack;
 extern uint32_t _heap_end;
 extern uint32_t _heap_start;
 
-void uart_puts(UART_Device dev, const char *str)
-{
-  while (*str) {
-    uart_putc(dev, *str);
-    str++;
-  }
-}
+/* void uart_puts(UART_Device dev, const char *str) */
+/* { */
+/*   while (*str) { */
+/*     uart_putc(dev, *str); */
+/*     str++; */
+/*   } */
+/* } */
 
-int _printf(const char *fmt, ...)
-{
-  va_list args;
-  va_start(args, fmt);
-  int len = vsnprintf(NULL, 0, fmt, args);
-  va_end(args);
+/* int _printf(const char *fmt, ...) */
+/* { */
+/*   va_list args; */
+/*   va_start(args, fmt); */
+/*   int len = vsnprintf(NULL, 0, fmt, args); */
+/*   va_end(args); */
 
-  char msg[len + 1];
-  memset(msg, 0, len + 1);
-  va_start(args, fmt);
-  vsnprintf(msg, len + 1, fmt, args);
-  va_end(args);
+/*   char msg[len + 1]; */
+/*   memset(msg, 0, len + 1); */
+/*   va_start(args, fmt); */
+/*   vsnprintf(msg, len + 1, fmt, args); */
+/*   va_end(args); */
 
-  uart_puts(UART_Device3, msg);
-  return len;
-}
+/*   uart_puts(UART_Device3, msg); */
+/*   return len; */
+/* } */
 
 const char DVP_Err_String[][64] = {"DVP_Err_Success", "DVP_Err_ConfigFail", "DVP_Err_AlreadyEnabled",
   "DVP_Err_AlreadyConfigured", "DVP_Err_NotEnabled", "DVP_Err_NoSuchDevice", "DVP_Err_DeviceOutOfRange"};
@@ -51,18 +52,13 @@ int main(void)
   uart_enable_device(UART_Device3, UART3_DEFAULT_MAPPING);
   uart_configure_device(UART_Device3, 9600, sysclock_frequency);
 
+  bind_printf(UART_Device3);
   DVP_Err status = dvp_enable_device(DVP_Device1, DVP_DEFAULT_MAPPING);
   if (status) {
-    _printf("DVP Device enable failed. Error code: %s\r\n", DVP_Err_String[status]);
+    printf_("DVP Device enable failed. Error code: %s\r\n", DVP_Err_String[status]);
     goto _nothing;
   }
 
-  mmem_init();
-  /* uint16_t *buff0 = allocm(sizeof(*buff0) * 16); */
-  /* uint16_t *buff1 = allocm(sizeof(*buff1) * 16); */
-
-  /* buff0 = (uint16_t *)0x20005000; */
-  /* buff1 = (uint16_t *)(0x20005000 + 128); */
   DVP_Config dvp_config = {
     .CR0 = {.DVP_ENABLE = 1,
       .DVP_VSYNC_POL = DVP_VSYNC_POL_LOW,
@@ -97,17 +93,17 @@ int main(void)
 
   status = dvp_configure_device(DVP_Device1, dvp_config);
   if (status) {
-    _printf("DVP Device configure failed. Error code: %s\r\n", DVP_Err_String[status]);
+    printf_("DVP Device configure failed. Error code: %s\r\n", DVP_Err_String[status]);
     goto _nothing;
   }
 
-  _printf("Initialization successful\r\n");
-  _printf("Syclock frequency: %d Hz\r\n", sysclock_frequency);
-  _printf("dvp_config.CR0.data: 0x%02x\r\n", dvp_config.CR0.data);
-  _printf("dvp_config.CR1.data: 0x%02x\r\n", dvp_config.CR1.data);
-  _printf("dvp_config.IFR.data: 0x%02x\r\n", dvp_config.CR1.data);
-  _printf("&buff0: 0x%08x\r\n", buff0);
-  _printf("&buff1: 0x%08x\r\n", buff1);
+  printf_("Initialization successful\r\n");
+  printf_("Syclock frequency: %d Hz\r\n", sysclock_frequency);
+  printf_("dvp_config.CR0.data: 0x%02x\r\n", dvp_config.CR0.data);
+  printf_("dvp_config.CR1.data: 0x%02x\r\n", dvp_config.CR1.data);
+  printf_("dvp_config.IFR.data: 0x%02x\r\n", dvp_config.CR1.data);
+  printf_("&buff0: 0x%08x\r\n", buff0);
+  printf_("&buff1: 0x%08x\r\n", buff1);
   dvp_dump_registers(DVP_Device1);
   wait_ms(500);
 
